014109fd8c1052241c6ba5cca3af8b7e5e35ee79
karlijc92 <karlijc92@gmail.com>
2026-01-27 14:36:18 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,17 @@
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
-import Steps from "@/components/Steps";
+import HowItWorks from "@/components/HowItWorks";
 import Pricing from "@/components/Pricing";
 import DisclaimerFooter from "@/components/DisclaimerFooter";
 
 export default function Index() {
   return (
-    <>
+    <div className="min-h-screen bg-black">
       <Hero />
       <Features />
-      <Steps />
+      <HowItWorks />
       <Pricing />
       <DisclaimerFooter />
-    </>
+    </div>
   );
 }
