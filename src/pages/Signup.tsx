57f2525a6c7fe6f263418a36f8ebffe7c0e071e1
karlijc92 <karlijc92@gmail.com>
2026-01-30 13:44:59 -0500

    Create Signup.tsx

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/SignUpForm";
+
+export default function Signup() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <SignUpForm />
+    </div>
+  );
+}
