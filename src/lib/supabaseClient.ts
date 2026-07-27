283531b49568279f28511b607688d3b16e45236f
karlijc92 <karlijc92@gmail.com>
2026-07-27 11:20:22 -0400

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rwicndsgbtqpytmyrbxu.supabase.co";
-const supabaseAnonKey = "sb_publishable_dp2X-rkqUF1TZyJt10yoZA_uSjjt-oT";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
